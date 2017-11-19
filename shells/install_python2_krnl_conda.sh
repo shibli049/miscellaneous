898fb628c0d2e529e6f9ddd4146235e666462e48
Tahmim Ahmed Shibli <shibli049@users.noreply.github.com>
2017-11-19 21:59:14 +0600

    Update install_python2_krnl_conda.sh

--- shells/install_python2_krnl_conda.sh
+++ shells/install_python2_krnl_conda.sh
@@ -1,3 +1,3 @@
-conda create -n ipykernel_py2 python=2 ipykernel
+conda create -n ipykernel_py2 python=2.7 anaconda
 source activate ipykernel_py2    # On Windows, remove the word 'source'
 python -m ipykernel install --user
