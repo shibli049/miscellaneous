c0fc9f5fb76f96dd3e7f64e6088a2ba4ab8f770b
Tahmim Ahmed Shibli <shibli049@users.noreply.github.com>
2018-02-10 00:06:41 +0600

    handled linux and windows platform.

--- python/local_fileserver.py
+++ python/local_fileserver.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+
 import http.server
 import socketserver
 import socket
-my_ip=socket.gethostbyname(socket.gethostname())
+import platform
+
+platform_system = platform.system()
+
+hostname = socket.gethostname()
+if 'Linux' == platform_system: 
+    from subprocess import call
+    print("host ips: ")
+    call(["hostname", "-I"])
+else:
+    my_ip=socket.gethostbyname(hostname)
+    print("host ips: {0}".format(my_ip))
+
+
 PORT = 8888
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
-print("serving at http://{0}:{1}/".format(my_ip,PORT))
+print("serving at http://{0}:{1}/".format(hostname,PORT))
 httpd.serve_forever()
