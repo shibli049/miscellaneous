41fae22f8bfe270a1cf5ac7b1f7bf901a52e7b80
Tahmim Ahmed Shibli <shibli049@users.noreply.github.com>
2018-07-26 16:52:40 +0600

    Updated the output.

--- java/CheckPolymorphism.java
+++ java/CheckPolymorphism.java
@@ -4,12 +4,12 @@
 public class CheckPolymorphism {
     public static void main(String[] args) {
         A a = new B();
-        System.out.println(a.name);
-        System.out.println(a.getName());
+        System.out.println(a.name); // Super-A
+        System.out.println(a.getName()); // Child-B
         
         B b = new B();
-        System.out.println(b.name);        
-        System.out.println(b.getName());
+        System.out.println(b.name); // Child-B        
+        System.out.println(b.getName()); // Child-B
         
     }
 }
