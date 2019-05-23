e29a55b51ac16e4e3ce811de0d7323c8ebbd8cba
Tahmim Ahmed Shibli <shibli049@users.noreply.github.com>
2019-05-23 12:04:44 +0600

    better error handling, and writing data.

--- python/simple_run_weekly.py
+++ python/simple_run_weekly.py
@@ -9,7 +9,7 @@ import json
 from subprocess import  run
 import logging
 logging.basicConfig(level=logging.INFO,
-                    format=' %(asctime)s - %(levelname)s - %(message)s')
+                    format=' %(asctime)s - %(levelname)s - %(lineno)d - %(message)s')
 
 ALERT_DAY='Thursday'
 filename = 'run_flag.json'
@@ -21,16 +21,18 @@ data = {'sent': False}
 try:
     current_day = date.today().strftime("%A")
     logging.info("current day: {}, alert day: {}".format(current_day, ALERT_DAY))
-    if current_day == ALERT_DAY:
-        with open(filename, 'r') as f:    
-            try: 
-                data = json.loads("\n".join(f.readlines()))
-            except (BaseException, Exception) as e:
-                logging.error("error: {}".format(e))
+    if current_day == ALERT_DAY:    
+        try: 
+            with open(filename, 'r') as f:
+            data = json.loads("\n".join(f.readlines()))
+        except (BaseException, Exception) as e:
+            logging.error("error: {}".format(e))
+            
         logging.info("data: {}".format(data))
         if  not data['sent']:
             run(CMD)
-            data['sent'] = True    
+            data['sent'] = True
+            
 except (BaseException, Exception) as e:
     logging.error("error: {}".format(e))
 finally:
